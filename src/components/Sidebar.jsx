a86276a9d093be1c8f3194666af54bfcd4aa017f
c protko <protkoc02@gmail.com>
2026-02-12 00:04:07 -0500

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,30 +1,50 @@
 import { useState, useCallback } from "react"
+
+
 /**
  * Renders an array of strings passed in that can be filtered and added to as an
  * unordered list.
  * @returns Component
  */
-export default function Sidebar() {
+
+
+
+export default function Sidebar({ initialMenuItems }) {
   let [newMenuItem, setNewMenuItem] = useState("")
+
   // TODO: 2 Using a state hook, maintain the current menu items as an array state.
   // let [menuItems, setMenuItems] = useState(initialMenuItems)
+
   let [filter, setFilter] = useState("")
+
   // Adds a single string passed in as parameter to the state element
   // "menuItems" that holds the set of current menu items.
+
   let addMenuItem = useCallback(() => {
     console.log("Added menu item")
+
     //   // TODO: 3. Add a new menu item to the correct variable associated with this class.
     //   // This involves adding a parameter and changing a class instance variable (props).
     //   setMenuItems([item, ...menuItems])
+
   }, [])
 
   // TODO: 4. Display ONLY the menu items that contain the filter element value
   // "term" in them. Each menu item should be an unordered list item wrapped in an unordered list (ul) element.
 
+
+
+
   // TODO: 1 Render inside the outer div an unordered list of the menu items, with each string in the array
   // its own item.
   return (
     <div>
+      <ul>
+        {initialMenuItems.map(item => (
+        <li>{item}</li>
+      ))}
+      </ul>
+
       <input
         type="text"
         id="newMenuItemValue"
